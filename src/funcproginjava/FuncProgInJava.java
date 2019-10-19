b7bcd2e63077ee88f3557f251981be607a7b3702
FrancaTM <francatm@outlook.com>
2019-10-19 13:41:32 -0300

    add imperative style example (find nemo)

--- src/funcproginjava/FuncProgInJava.java
+++ src/funcproginjava/FuncProgInJava.java
@@ -20,5 +20,29 @@ public class FuncProgInJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // FindNemo demo
+        List<String> names = Arrays.asList("Dory", "Gill", "Bruce", "Nemo", "Darla", "Marlin", "Jacques");
+
+        findNemoImperative(names);
+
     }
+
+    // Imperative Style - Example
+    public static void findNemoImperative(List<String> names) {
+        boolean found = false;
+
+        for (String name : names) {
+            if (name.equals("Nemo")) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Found Nemo");
+        } else {
+            System.out.println("Sorry, Nemo not found");
+        }
+    }
+
 }
