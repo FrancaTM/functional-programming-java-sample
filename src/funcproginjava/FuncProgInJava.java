74004c7f0c2558a980037c704b00f9450b32f468
FrancaTM <francatm@outlook.com>
2019-10-19 13:33:13 -0300

    initial commit

---
+++ src/funcproginjava/FuncProgInJava.java
@@ -0,0 +1,24 @@
+// An easier path to functional programming in Java
+// Source: https://developer.ibm.com/articles/j-java8idioms1/
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package funcproginjava;
+
+import java.util.*;
+
+/**
+ *
+ * @author francatm
+ */
+public class FuncProgInJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
